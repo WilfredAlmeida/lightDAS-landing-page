059a0a4ca8065559dce36f296434e48cb3faeb31
Pritipriya Singh <mail2pritipriya@gmail.com>
2024-02-27 21:16:29 +0530

    all the major changes

--- app/Components/TweetEmbed.tsx
+++ app/Components/TweetEmbed.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Tweet } from 'react-tweet'
 import * as containerStyles from "./container.module.css"
 
-const TweetEmbed = ({id}: string) => {
+const TweetEmbed = (id: string) => {
   return (
     <div className={containerStyles.container}>
     <Tweet id={id}/>
